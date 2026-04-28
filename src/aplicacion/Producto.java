e45abcabb065483753d422aebbbdc667dbc81840
Diego4360 <109175586+Y4360@users.noreply.github.com>
2026-04-28 10:21:08 +0200

    Creación clase y constructores y getters setters

---
+++ src/aplicacion/Producto.java
@@ -0,0 +1,53 @@
+public class Producto {
+
+    private String nombre;
+    private String ID;
+    private float precioUnidad;
+    private String proveedor;
+
+    // Constructor vacío
+    public Producto() {
+    }
+
+    // Constructor con parámetros
+    public Producto(String nombre, String ID, float precioUnidad, String proveedor) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.precioUnidad = precioUnidad;
+        this.proveedor = proveedor;
+    }
+
+    // Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public float getPrecioUnidad() {
+        return precioUnidad;
+    }
+
+    public void setPrecioUnidad(float precioUnidad) {
+        this.precioUnidad = precioUnidad;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+}
