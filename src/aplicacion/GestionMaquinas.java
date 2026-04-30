37ad0b36a2d409ca2330c7ca99af65885ad2a861
JorgeFerroS <jorge.ferro@rai.usc.es>
2026-04-30 16:41:24 +0200

    Añadida conexión con GestionMaquinas

--- src/aplicacion/GestionMaquinas.java
+++ src/aplicacion/GestionMaquinas.java
@@ -1,13 +1,28 @@
 package aplicacion;
 
+import java.util.HashMap;
+
 public class GestionMaquinas {
 
 	gui.FachadaGUI fgui;
 	baseDatos.FachadaBD fbd;
+	HashMap<String, MaquinaExpendedora> maquinasExpendedoras;
 	
 	public GestionMaquinas(gui.FachadaGUI fgui, baseDatos.FachadaBD fbd) {
 		this.fgui = fgui;
 		this.fbd = fbd;
+		
+		maquinasExpendedoras = new HashMap<String, MaquinaExpendedora>();
+	}
+	
+	public void registrarVenta(String maquina, int producto, int unidades) throws IllegalArgumentException {
+		
+		if(maquinasExpendedoras.get(maquina) == null) {
+			throw new IllegalArgumentException("No existe ninguna máquina identificada como " + maquina + ".");
+		}
+		
+		maquinasExpendedoras.get(maquina).venta(producto, unidades);
+		
 	}
 	
 }
