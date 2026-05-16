0f8dfcb3a2f38d63cae32d6eb0aea9067cc37764
FernandoJavierAG <fernandojavier.alvarez@rai.usc.es>
2026-05-16 22:14:57 +0200

    Esbozo de pruebas II - Ampliación y setters.

--- test/aplicacion/VentaTest.java
+++ test/aplicacion/VentaTest.java
@@ -24,13 +24,13 @@ class VentaTest {
 	MaquinaExpendedora maquina;
 	Producto producto;
 	
+	// Pendiente reemplazar por Mocks.
 	@BeforeAll
 	static void setUpAll() {
 		maqReferencia = new MaquinaExpendedora("MaquinaDe-PRUEB4", 100);
 		prodReferencia = new Producto();
 	}
 	
-	@AfterAll
 	
 	@BeforeEach
 	void setUpEach() throws Exception {
@@ -46,7 +46,7 @@ class VentaTest {
 
 	@ParameterizedTest
 	@DisplayName("Comportamiento general del constructor.")
-	@Tag("Tests sobre constructores")
+	@Tag("TestsSobreConstructores")
 	@CsvSource({"1", "100"})
 	void testConstructorValido(int unidades) {
 		// Arrange
@@ -61,11 +61,93 @@ class VentaTest {
 		);
 		
 	}
+	
+	@Test
+	@DisplayName("Comportamiento general de setMaquina.")
+	@Tag("IntegridadDeDatos")
+	void testSetMaquina() {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 10);
+		MaquinaExpendedora nuevaMaquina = new MaquinaExpendedora("VENDTEST-1001", 100);
+		// Act
+		
+		v1.setMaquina(nuevaMaquina);
+		
+		// Assert
+		assertEquals(v1.getMaquina(), nuevaMaquina, "Fallo al modificar maquina.");
+	}
+	
+	@Test
+	@DisplayName("Comportamiento general de setProducto.")
+	@Tag("IntegridadDeDatos")
+	void testSetProducto() {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 10);
+		Producto nuevoProducto = new Producto();
+		// Act
+		
+		v1.setProducto(nuevoProducto);
+		
+		// Assert
+		assertEquals(v1.getProducto(), nuevoProducto, "Fallo al modificar producto.");
+	}
+	
+	@ParameterizedTest
+	@DisplayName("Comportamiento general de setUnidades.")
+	@Tag("IntegridadDeDatos")
+	@CsvSource({"1", "10", "1000"})
+	void testSetUnidades(int unidades) {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 133);
+		// Act
+		
+		v1.setUnidades(unidades);
+				
+		//Assert
+		assertEquals(v1.getUnidades(), unidades, "Fallo al modificar unidades.");
+	}
+	
+	@Test
+	@DisplayName("Comportamiento general de setFecha.")
+	@Tag("IntegridadDeDatos")
+	void testSetFecha() {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 10);
+		Instant nuevaFecha = Instant.now().minus(2, ChronoUnit.HOURS);
+		// Act
+		
+		v1.setFecha(nuevaFecha);
+				
+		//Assert
+		assertEquals(v1.getFecha(), nuevaFecha, "Fallo al modificar fecha.");
+	}
+	
 
 	@ParameterizedTest
-	@DisplayName("Constructor con datos no validos")
-	@Tag("Tests sobre constructores")
-	@CsvSource({"-10", "-1", "0", })
+	@DisplayName("setUnidades no válidas.")
+	@Tag("IntegridadDeDatos")
+	@CsvSource({"-10", "-1", "0"})
+	void testsetUnidadesNoValido(int unidades) {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 10);
+		
+		// Act
+		
+		//Assert
+		IllegalArgumentException iae = assertThrows(
+				IllegalArgumentException.class,
+				() -> {v1.setUnidades(unidades);},
+				"Excepción no generada introducir unidades no-válidas.");
+		assertEquals(
+				iae.getMessage(), "Unidades no-validas introducidas.", 
+				"Generada excepción inesperada.");
+		
+	}
+	
+	@ParameterizedTest
+	@DisplayName("Constructor con unidades no validas")
+	@Tag("TestsSobreConstructores")
+	@CsvSource({"-10", "-1", "0"})
 	void testConstructorNoValido(int unidades) {
 		// Arrange
 		
@@ -84,7 +166,7 @@ class VentaTest {
 	
 	@Test
 	@DisplayName("setMaquina a null")
-	@Tag("Integridad de datos")
+	@Tag("IntegridadDeDatos")
 	void testsetMaquinaNull() {
 		// Arrange
 		Venta v1 = new Venta(maquina, producto, 10);
@@ -103,7 +185,7 @@ class VentaTest {
 	
 	@Test
 	@DisplayName("Constructor con maquina null")
-	@Tag("Tests sobre constructores")
+	@Tag("TestsSobreConstructores")
 	void testConstructorMaquinaNull() {
 		// Arrange
 		
@@ -121,7 +203,7 @@ class VentaTest {
 	
 	@Test
 	@DisplayName("setProducto a null")
-	@Tag("Integridad de datos")
+	@Tag("IntegridadDeDatos")
 	void testsetProductoNull() {
 		// Arrange
 		Venta v1 = new Venta(maquina, producto, 10);
@@ -132,7 +214,7 @@ class VentaTest {
 		IllegalArgumentException iae = assertThrows(
 				IllegalArgumentException.class,
 				() -> {v1.setProducto(null);},
-				"Excepción no generada introducir un producto nulo.");
+				"Excepción no generada al introducir un producto nulo.");
 		assertEquals(
 				iae.getMessage(), "Producto nulo introducido.", 
 				"Generada excepción inesperada.");
@@ -141,7 +223,7 @@ class VentaTest {
 	
 	@Test
 	@DisplayName("Constructor con producto null")
-	@Tag("Tests sobre constructores")
+	@Tag("TestsSobreConstructores")
 	void testConstructorProductoNull() {
 		// Arrange
 		
@@ -155,6 +237,46 @@ class VentaTest {
 		assertEquals(
 				iae.getMessage(), "Producto nulo introducido.", 
 				"Generada excepción inesperada.");
+	}
+	
+	@Test
+	@DisplayName("setFecha a null")
+	@Tag("IntegridadDeDatos")
+	void testSetFechaNull() {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 10);
+		
+		// Act
+		
+		//Assert
+		IllegalArgumentException iae = assertThrows(
+				IllegalArgumentException.class,
+				() -> {v1.setFecha(null);},
+				"Excepción no generada al introducir una fecha nula.");
+		assertEquals(
+				iae.getMessage(), "Fecha nula introducida.", 
+				"Generada excepción inesperada.");
+		
+	}
+	
+	@Test
+	@DisplayName("setFecha a futuro")
+	@Tag("IntegridadDeDatos")
+	void testSetFechaFuturo() {
+		// Arrange
+		Venta v1 = new Venta(maquina, producto, 10);
+		
+		// Act
+		
+		//Assert
+		IllegalArgumentException iae = assertThrows(
+				IllegalArgumentException.class,
+				() -> {v1.setFecha(Instant.now().plus(1, ChronoUnit.HOURS));},
+				"Excepción no generada al introducir una fecha futura.");
+		assertEquals(
+				iae.getMessage(), "Fecha futura introducida.", 
+				"Generada excepción inesperada.");
+		
 	}
 		
 	@Test
@@ -172,10 +294,12 @@ class VentaTest {
 				v2 = v1,
 				v3 = new Venta(m2, p2, 4),
 				v4 = new Venta(m1, p1, 10),
-				v5 = new Venta(m1, p1, 10);
+				v5 = new Venta(m1, p1, 10),
+				v6 = null;
+		String v7 = "notAVenta";
 		
-		Instant fecha1 = Instant.now(),
-				fecha2 = fecha1.plus(2, ChronoUnit.HOURS);
+		Instant fecha1 = Instant.now().minus(1, ChronoUnit.HOURS),
+				fecha2 = fecha1.minus(1, ChronoUnit.HOURS);
 		
 		// Act
 		
@@ -191,7 +315,9 @@ class VentaTest {
 			() -> assertEquals(v1, v2, "Una venta no es idéntica a sí misma tras aliasing."),
 			() -> assertNotEquals(v1, v3, "Una venta es identica a otra con productos y máquinas distintos."),
 			() -> assertNotEquals(v1, v4, "Una venta es idéntica a otra con fecha distinta."),
-			() -> assertEquals(v1, v5, "Una venta no es idéntica a otra venta con la misma fecha, máquina y producto.")
+			() -> assertEquals(v1, v5, "Una venta no es idéntica a otra venta con la misma fecha, máquina y producto."),
+			() -> assertNotEquals(v1, v6, "Una venta inicializada es idéntica a una nula."),
+			() -> assertNotEquals(v1, v7, "Una venta es idéntica a un objeto de otra clase.")
 			);
 	}
 }
